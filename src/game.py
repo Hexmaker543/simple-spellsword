a68c37547e20b4b84551a859f836bd9ffb89509c
Hexmaker <hexmaker543@gmail.com>
2026-03-03 11:41:28 -0500

    window is drawn now, settings class made

--- src/game.py
+++ src/game.py
@@ -1,6 +1,28 @@
 import pygame, sys
+from settings import Settings
 
 
 class Game:
     def __init__(self):
-        pass
+        pygame.init()
+        self.settings = Settings()
+        pygame.display.set_caption(self.settings.WINDOW_TITLE)
+        self._initialize_screen()
+
+    def _initialize_screen(self):
+        self.screen = pygame.display.set_mode(self.settings.BASE_WINDOW_SIZE)
+        self.rect = self.screen.get_rect()
+
+    def run(self):
+        while True:
+            self._handle_input()
+            self.screen.fill(self.settings.BASE_WINDOW_BACKGROUND_COLOR)
+            pygame.display.flip()
+
+    def _handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+
+game = Game()
+game.run()
---
+++ src/settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    def __init__(self):
+        # Color
+        self.BLACK = (0,0,0)
+        self.WHITE = (255,255,255)
+        self.RED = (255,0,0)
+        self.GREEN = (0,255,0)
+        self.BLUE = (0,0,255)
+
+        # Screen
+        self.WINDOW_TITLE = "Simple Spellsword"
+        self.BASE_WINDOW_SIZE = (896, 896)
+        self.BASE_WINDOW_BACKGROUND_COLOR = self.BLACK
